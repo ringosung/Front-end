c17c2bc7b6d30b515900353a598e180cb376745b
Ringo Sung <ringosung@gmail.com>
2022-04-10 02:45:53 +0800

    add some error handler for routing

--- app.js
+++ app.js
@@ -17,10 +17,6 @@ if(process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
-app.use((req, res, next) => {
-    console.log('Hello from the middleware');
-    next();
-});
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -51,6 +47,14 @@ app.delete('/api/v1/tours/:id', deleteTour); */
 
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `can't find ${req.originalUrl} on this server`
+    })
+})
+
 
 // 4) start the server
 
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 const slugify = require('slugify');
+const validator = require('validator')
 
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
         unique: true,
-        trim: true
+        trim: true,
+        maxlength: [40, 'A tour name must be less or equal than 40 characters'],
+        minlength: [10, 'A tour name must be greater than 10 characters'],
+       // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration: {
@@ -20,12 +24,18 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty: {
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
     },
 
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must be above 1.0'],
+        max: [1, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
         type: Number,
@@ -35,7 +45,15 @@ const tourSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function(val) {
+            return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) must be below regular price'
+        }
+    },
     summary:{
         type: String,
         trim: true,
