19547bae6f6a47db6f327ebba463311852610c13
Ringo Sung <ringosung@gmail.com>
2022-05-02 04:49:50 +0800

    factory the CURD routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const factory = require('../controllers/handlerFactory')
 
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
@@ -10,18 +11,7 @@ const filterObj = (obj, ...allowedFields) => {
     return newObj;
 }
 
-exports. getAllUsers = catchAsync(async (req, res, next) => {
-    const users = await User.find();
 
-    // Send Response
-    res.status(200).json({
-    status: 'success', 
-    results: users.length,
-    data: {
-            users
-        }
-    });
-});
 
 exports.updateMe = catchAsync(async(req, res, next) => {
     // 1) Create error if user wants to update password
@@ -55,30 +45,16 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
     })
 })
 
-exports. getUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined'
-    })
-}
+exports. getAllUsers = factory.getAll(User);
+exports. getUser = factory.getOne(User);
 
-exports. updateUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined'
-    })
-}
-
-exports. deleteUser = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined'
-    })
-}
+// Do not update password with this
+exports. updateUser = factory.updateOne(User);
+exports. deleteUser = factory.deleteOne(User);
 
 exports. createUser = (req, res) => {
     res.status(500).json({
         status: 'error',
-        message: 'This route is not yet defined'
+        message: 'Please use signup route instead'
     })
 }
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -9,8 +9,14 @@ router.route('/')
 .post(
     authController.protect, 
     authController.restrictTo('user'),
+    reviewController.setTourUserIds,
     reviewController.createReview
     );
+
+router.route('/:id')
+.get(reviewController.getReview)
+.patch(reviewController.updateReview)
+.delete(reviewController.deleteReview);
 
 module.exports = router;
 
